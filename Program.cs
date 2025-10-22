4806d21284d53ba1c86056711cafc971468ae767
Nick Grove <nickgrove.se@gmail.com>
2025-10-22 01:16:09 -0400

    Desktop App Beginning

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.Run(new Merlin());
+    }
+}
+
+class Merlin : Form
+{
+    public Merlin()
+    {
+        this.Text = "merlin";
+        this.Size = new Size(1000, 800);
+        this.StartPosition = FormStartPosition.CenterScreen;
+
+        Button btn = new Button
+        {
+            Text = "Click Me!",
+            Size = new Size(150, 50),
+            Location = new Point(125, 60),
+            Font = new Font("Segoe UI", 12, FontStyle.Bold)
+        };
+
+        btn.Click += (sender, e) => 
+        {
+            MessageBox.Show("Hello World!", "Message");
+        };
+
+        this.Controls.Add(btn);
+    }
+}
